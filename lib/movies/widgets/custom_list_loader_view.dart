849aa3d9e5c42b5a9079a541497e7598ee30e377
Mahmud Basunia <mahmud.basunia@gmail.com>
2022-09-07 21:40:49 +0600

    polishes custom loader

--- lib/movies/widgets/custom_list_loader_view.dart
+++ lib/movies/widgets/custom_list_loader_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_buzz/utils/responsive_util.dart';
 import 'package:skeleton_loader/skeleton_loader.dart';
 import 'package:skeletons/skeletons.dart';
 
@@ -7,6 +8,36 @@ class CutomListLoaderView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return SkeletonGridLoader(
+      builder: Card(
+        color: Colors.transparent,
+        child: GridTile(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 50,
+                height: 10,
+                color: Colors.white,
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: 70,
+                height: 10,
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+      items: 8,
+      itemsPerRow: MediaQuery.of(context).orientation.isLandScape ? 4 : 2,
+      period: const Duration(seconds: 2),
+      highlightColor: const Color(0xFFE3515B),
+      direction: SkeletonDirection.ltr,
+      childAspectRatio: 1,
+    );
+
 // return ListView.builder(
 //   physics: NeverScrollableScrollPhysics(),
 //   itemCount: 5,
